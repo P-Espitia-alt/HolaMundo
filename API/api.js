2197ab76f3a07bbb28630dd03e8ce90b4587d71a
JD-ESPITIA <juano.o@outlook.es>
2025-07-01 19:48:10 -0500

    modifico js para insert src desde código, y agrego tag img en html y agrego script de jquery

--- API/api.js
+++ API/api.js
@@ -25,16 +25,6 @@ function imagen() {
     }
 }
 
-<img src="document.get" alt="Logo de HTML5"
-    srcset="imagen_1200w.jpg 1200w,
-             imagen_900w.jpg 900w,
-             imagen_400w.jpg 400w"
-    sizes="(width > 900px) 900px,
-            ((width > 480px) and (width <= 800px)) 480px,
-            ((width <= 480px)) 320px"
-
-/>
-
 
 
 
@@ -54,7 +44,7 @@ function pz() {
         })
         .then(data => {
             console.log(data);
-            mostrarImagen(data.message);
+            mostrarImagen(data.image);
         })
         .catch(error => console.error('Error:', error));
 }
@@ -63,4 +53,16 @@ function imagenes() {
     for (let i = 0; i < 5; i++) {
         pz();
     }
+}
+
+
+/// Esta función mostrará las imagenes usando una url
+function mostrarImagen(url) {
+
+    console.log("url que llega:",url);
+
+    // usar la url para modficar src del elemento img en el html
+    $("#imagen").attr('src', url);
+
+
 }
